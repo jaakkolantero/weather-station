e76ea5bd622bb3d90b4a40e9978a1669de65b661
UFo_x <tero.jaakkola1@gmail.com>
2020-02-21 09:32:18 +0200

    fix: 🐛 naming
    components to PascalCase

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import { NextPage } from "next";
 
 const Layout: NextPage = ({ children }) => {
